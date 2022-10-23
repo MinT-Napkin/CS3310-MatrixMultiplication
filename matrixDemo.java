93a6896a4af9faa1cecb1c2e627bffba4136d34a
Minh Tran <mtran8011@yahoo.com>
2022-10-22 20:24:33 -0700

    remove files

--- matrixDemo.java
+++ matrixDemo.java
@@ -1,8 +1,19 @@
 public class matrixDemo {
     public static void main(String args[]) {
-    
         
 
 
+    }
+
+    public int[][] classicalMultiplication(int m1[][], int m2[][]){
+        return null;
+    }
+
+    public int[][] naiveMultiplication(int m1[][], int m2[][]){
+        return null;
+    }
+
+    public int[][] strassenMultiplication(int m1[][], int m2[][]){
+        return null;
     }
 }
