fc47ad5ebeb346a0722a69fa73cf25c6c1cea70e
Minh Tran <mtran8011@yahoo.com>
2022-10-23 20:07:59 -0700

    misinterpreted matrix, redid classical implementation

--- matrixDemo.java
+++ matrixDemo.java
@@ -1,4 +1,4 @@
-import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class matrixDemo {
     public static void main(String args[]) {
@@ -11,13 +11,23 @@ public class matrixDemo {
         int[][] matrix2 = {
             {1, 2, 3},
             {1, 2, 3},
-            {1, 2, 3}
+            {1, 2, 3},
         };
 
         int[][] matrix3 = classicalMultiplication(matrix, matrix2);
-        for(int row = 0; row < 3; row++)
+        for(int row = 0; row < matrix3.length; row++)
         {
-            for(int col = 0; col < 3; col++)
+            for(int col = 0; col < matrix3.length; col++)
+            {
+                System.out.println(matrix3[row][col]);
+            }
+            System.out.println();
+        }
+
+        matrix3 = naiveMultiplication(matrix, matrix2);
+        for(int row = 0; row < matrix3.length; row++)
+        {
+            for(int col = 0; col < matrix3.length; col++)
             {
                 System.out.println(matrix3[row][col]);
             }
@@ -26,18 +36,24 @@ public class matrixDemo {
 
     }
 
+    // all methods assume n is a power of 2
     public static int[][] classicalMultiplication(int m1[][], int m2[][]){
 
         int size = m1.length;
         int[][] result = new int[size][size];
 
 
-        for(int row = 0; row < 3; row++)
+        for(int row = 0; row < size; row++)
         {
-            for(int col = 0; col < 3; col++)
+            for(int col = 0; col < size; col++)
             {
-                result[row][col] = m1[row][col]*m2[row][col];
+                int multSum = 0;
+                for(int index = 0; index < size; index++)
+                {
+                    multSum += m1[row][index]*m2[index][col];
+                }
                 // System.out.println(result[row][col]);
+                result[row][col] = multSum;
             }
         }
 
@@ -46,7 +62,25 @@ public class matrixDemo {
     }
 
     public static int[][] naiveMultiplication(int m1[][], int m2[][]){
-        return null;
+
+        int size = m1.length;
+        int[][] result = new int[size][size];
+        int middle = size/2;
+
+        int[][] tl;
+        int[][] tr;
+        int[][] bl;
+        int[][] br;
+        // new int[middle][middle];
+
+        for(int row = 0; row < middle; row++)
+        {
+            // tl = Arrays.copyOfRange(, row, row)
+        }
+        // int[] newArray = Arrays.copyOfRange(oldArray, startIndex, endIndex);
+        
+
+        return result;
     }
 
     public static int[][] strassenMultiplication(int m1[][], int m2[][]){
