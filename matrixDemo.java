102abf7da84ccaecb899cf615f7bc58900d59d1f
Minh Tran <mtran8011@yahoo.com>
2022-11-04 23:00:35 -0700

    Extension and trimming matrixes to power of 2

--- matrixDemo.java
+++ matrixDemo.java
@@ -1,5 +1,6 @@
 import java.lang.Math;
 import java.util.Arrays;
+import java.util.Random;
 
 public class matrixDemo {
     // private static int counter = 0;
@@ -29,9 +30,16 @@ public class matrixDemo {
             {1, 2, 3},
             {1, 2, 3}
         };
-
+        
+        // System.out.println("That took " + (endTime - startTime) + " milliseconds");
+        
+        long startTime = System.nanoTime();
         int[][] resultMatrix = classicalMultiplication(matrix, matrix2);
-        System.out.println("Classical Result: ");
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("Classical Result (Execution Time: " + (elapsedTime) + " nanoseconds)");
+
         for(int row = 0; row < resultMatrix.length; row++)
         {
             for(int col = 0; col < resultMatrix.length; col++)
@@ -40,11 +48,14 @@ public class matrixDemo {
             }
             System.out.println();
         }
-        
-        resultMatrix = naiveMultiplication(matrix, matrix2);
         System.out.println();
+        
+        startTime = System.nanoTime();
+        resultMatrix = naiveMultiplication(matrix, matrix2);
+        endTime = System.nanoTime();
+        elapsedTime = endTime - startTime;
 
-        System.out.println("Naive Result: ");
+        System.out.println("Naive Result: (Execution Time: " + (elapsedTime) + " nanoseconds)");
         for(int row = 0; row < resultMatrix.length; row++)
         {
             for(int col = 0; col < resultMatrix.length; col++)
@@ -53,9 +64,50 @@ public class matrixDemo {
             }
             System.out.println();
         }
-        
+        System.out.println();
         // System.out.println("\nCounter: " + counter);
+        
+        // Testing random matrixes generation
+        for(int i = 0; i < 1; i++)
+        {
+            System.out.println("Random 2D Matrix: ");
+            int[][] rArr = populateRandom2DMatrix(10, 10);
+    
+            for(int row = 0; row < rArr.length; row++)
+            {
+                for(int col = 0; col < rArr.length; col++)
+                {
+                    System.out.print(rArr[row][col] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        
+        // Testing extendMatrix and trimMatrix methods
+        System.out.println("Before Extension: " + matrix3.length);
+        System.out.println("After Extension: " + extendMatrix(matrix3).length);
 
+        int[][] test = extendMatrix(matrix3);
+
+        for (int i = 0; i < test.length; i++) {
+            for (int j = 0; j < test[0].length; j++) {
+                System.out.print(test[i][j]);
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Before Trim: " + test.length);
+        System.out.println("After Trim: " + trimMatrix(test).length);
+
+        int[][] test2 = trimMatrix(test);
+        for (int i = 0; i < test2.length; i++) {
+            for (int j = 0; j < test2[0].length; j++) {
+                System.out.print(test2[i][j]);
+            }
+            System.out.println();
+        }
     }
 
     // all methods assume n is a power of 2
@@ -165,4 +217,85 @@ public class matrixDemo {
     public static int[][] strassenMultiplication(int m1[][], int m2[][]){
         return null;
     }
+
+    // For testing purposes
+    public static int[][] populateRandom2DMatrix(int n1, int n2)
+    {
+        Random rand = new Random();
+        int randNum = rand.nextInt(19) - 9;
+
+        int[][] result = new int[n1][n2];
+
+        for (int row = 0; row < result.length; row++) {
+            for(int col = 0; col < result.length; col++)
+            {
+                result [row][col] = randNum;
+                randNum = rand.nextInt(19) - 9;
+            }
+        }
+
+        return result;
+    }
+
+    public static int[][] extendMatrix(int[][] matrix)
+    {
+        int size = matrix.length;
+
+        if(isPowerOfTwo(size))
+        {
+            return matrix;
+        }
+
+        while(!isPowerOfTwo(size))
+        {
+            size++;
+        }
+
+        int[][] result = new int[size][size];
+
+        // Only fills in the available data and the rest are 0's
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                result[i][j] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public static int[][] trimMatrix(int[][] matrix)
+    {
+        int size = matrix.length;
+
+        // Detects a rows of 0's, which is the point of extension
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if(matrix[i][j] != 0)
+                {
+                    j = matrix.length;
+                }
+                else if (j == matrix.length-1)
+                {
+                    size = i;
+                }
+            }
+        }
+
+        int[][] result = new int[size][size];
+        
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result.length; j++) {
+                result[i][j] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    /* Checks if the number is a power of 2 */
+    public static boolean isPowerOfTwo(int n)
+    {
+        return (n != 0) && ((n & (n - 1)) == 0);
+    }
+     
 }
